317995f794a073b3a969f5d60952ec8497488ac2
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-02-20 19:44:40 +0300

    hange of projects

--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+// import axios from "axios";
+
+// axios.defaults.withCredentials = true;
+// axios.defaults.baseURL = "https://gabbyblog.herokuapp.com/";
 
 createApp(App).use(store).use(router).mount("#app");
