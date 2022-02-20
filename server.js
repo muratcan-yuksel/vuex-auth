1521d3421cf19f7ab0889b9d6fc3ce383250c02f
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-02-20 22:24:26 +0300

    .

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 3000;
 
+app.use(cors());
+
+app.use(express.json());
+
 app.post("/login", (req, res) => {
-  res.json({
-    message: "hellooo",
-  });
+  const USERNAME = "user";
+  const PASSWORD = "123";
+
+  // const username = req.body;
+  // const password = req.body;
+  const { username, password } = req.body;
+  if (username === USERNAME && password === PASSWORD) {
+    //we're good, 200 ok
+    res.json({
+      message: "hellooo",
+    });
+  } else {
+    res.status(403);
+    res.json({
+      message: "invalid login info",
+    });
+  }
 });
 
 app.listen(port, () => {
